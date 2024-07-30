19b47695b31c8f3415a7e2f3c9735babb3776ffd
Jacob Warren <jacobiwarren@gmail.com>
2024-07-30 11:16:06 -0500

    test; prints a card

---
+++ blackjack.java
@@ -0,0 +1,14 @@
+/*
+    Classes we might need:
+    Controller - takes input from user
+
+
+*/ 
+
+class blackjack
+{
+    public static void main(String []args)
+    {
+        System.out.println("\033[0;37m" + " ___\n|K  |\n| " + "\033[0;91m" + "♡" + "\033[0;37m" + " |\n|  K|\n ̅ ̅ ̅ ");
+    }
+}
